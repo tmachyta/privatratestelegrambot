2d9841f2c2fa2a01c57909d4a334de85e9713a9f
Taras Machyta <taras.machyta.sa@gmail.com>
2024-12-06 23:49:54 +0200

    Updates

--- src/test/java/currencies/privatratecurrencies/PrivatratecurrenciesApplicationTests.java
+++ src/test/java/currencies/privatratecurrencies/PrivatratecurrenciesApplicationTests.java
@@ -3,7 +3,7 @@ package currencies.privatratecurrencies;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = PrivatratecurrenciesApplicationTests.class)
 class PrivatratecurrenciesApplicationTests {
 
     @Test
